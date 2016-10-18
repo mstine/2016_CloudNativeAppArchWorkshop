bc303095f6ec486e1ee90a7678ee10c2aac73a58
Matt Stine <mstine@pivotal.io>
2016-10-18 15:59:30 +0100

    update completed fortune-teller to match new labs

--- labs/complete/fortune-teller/scripts/create_services.sh
+++ labs/complete/fortune-teller/scripts/create_services.sh
@@ -1,3 +1,6 @@
-cf cups config-service -p '{"uri":"http://config-server-fluxional-suttee.cfapps.io"}'
-cf cups service-registry -p '{"uri":"http://eureka-unprevalent-toper.cfapps.io"}'
-cf cs elephantsql turtle fortunes-db
+#!/usr/bin/env bash
+
+cf cs p-mysql 100mb fortunes-db
+cf cs p-config-server standard config-server -c '{"git": { "uri": "https://github.com/mstine/config-repo" } }'
+cf cs p-service-registry standard service-registry
+cf cs p-circuit-breaker-dashboard standard circuit-breaker-dashboard
