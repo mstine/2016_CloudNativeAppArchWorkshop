6590cc192dc3dbe07df5012b0fb83860b8602d59
Matt Stine <mstine@pivotal.io>
2017-05-09 15:43:00 +0100

    update security settings

--- labs/complete/hello-spring-boot/src/test/java/io/pivotal/spring/HelloSpringBootApplicationTests.java
+++ labs/complete/hello-spring-boot/src/test/java/io/pivotal/spring/HelloSpringBootApplicationTests.java
@@ -2,13 +2,13 @@ package io.pivotal.spring;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = HelloSpringBootApplication.class)
-@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class HelloSpringBootApplicationTests {
 
 	@Test
